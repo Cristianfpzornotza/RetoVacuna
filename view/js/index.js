e9da33a05cc8895c2ff683d77eecba53afd546a9
phorcajada <p_horcajada@fpzornotza.com>
2022-01-13 10:06:57 +0100

    graficas

--- view/js/index.js
+++ view/js/index.js
@@ -1,6 +1,93 @@
 document.addEventListener("DOMContentLoaded",function(){
 
+    estilosmenu();
+    graficaBizkaia();
+    graficaGipuzkoa();
+    miGraficaAra();
+})
 
+function graficaBizkaia(){
+    let miCanvas=document.getElementById("miGraficaBizk").getContext("2d");
+
+    var xValues = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
+
+        var chart = new Chart(miCanvas,{
+        type: "line",
+        data: {
+            labels: xValues,
+            datasets: [{ 
+            label:"Positivos",
+            data: ["255","871","552","661","431","550","620","700","759","639","721","809"],
+            backgroundColor:"red",
+            borderColor:"red",
+            fill: false
+            }, { 
+                label:"Fallecidos",
+            data: ["120","50","100","200","20","88","34","91","107","200","300","500"],
+            backgroundColor:"black",
+            borderColor:"black",
+            fill: false
+            }]
+        }
+ 
+});
+}
+
+function graficaGipuzkoa(){
+    let miCanvas=document.getElementById("miGraficaGipu").getContext("2d");
+
+    var xValues = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
+
+        var chart = new Chart(miCanvas,{
+        type: "line",
+        data: {
+            labels: xValues,
+            datasets: [{ 
+            label:"Positivos",
+            data: ["332","128","476","543","675","411","700","721","659","539","421","709"],
+            backgroundColor:"red",
+            borderColor:"red",
+            fill: false
+            }, { 
+                label:"Fallecidos",
+            data: ["100","75","150","220","120","58","74","121","20","190","300","400"],
+            backgroundColor:"black",
+            borderColor:"black",
+            fill: false
+            }]
+        }
+ 
+});
+}
+
+function miGraficaAra(){
+    let miCanvas=document.getElementById("miGraficaAra").getContext("2d");
+
+    var xValues = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
+
+        var chart = new Chart(miCanvas,{
+        type: "line",
+        data: {
+            labels: xValues,
+            datasets: [{ 
+            label:"Positivos",
+            data: ["432","228","276","443","575","311","500","421","659","739","821","409"],
+            backgroundColor:"red",
+            borderColor:"red",
+            fill: false
+            }, { 
+                label:"Fallecidos",
+            data: ["150","21","50","320","220","158","134","221","420","190","20","100"],
+            backgroundColor:"black",
+            borderColor:"black",
+            fill: false
+            }]
+        }
+ 
+});
+}
+
+function estilosmenu(){
     document.getElementById("citavacunas").addEventListener('mouseover', function(){
         document.querySelector("#citavacunas .halo").style.fontSize = "50px";
         document.querySelector("#citavacunas .halo2").style.fontSize = "18px";
@@ -35,17 +122,18 @@ document.addEventListener("DOMContentLoaded",function(){
         document.querySelector("#hospitalzona .btncita").style.width = "75px";
 
     });
+}
+
+
+
 
 
 
     
-})
 
-/*$( document ).ready(function() {
+
+
     
-$("#citavacunas").hover(function(){
-        alert("hola");
-    });
 
-});*/
+
 
